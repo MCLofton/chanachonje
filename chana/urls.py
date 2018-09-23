ae8ffca6baeec86f5f3a2707b4d1ae1a20a8706b
TralahM <mmtiese@gmail.com>
2018-09-23 23:10:47 +0300

    New version

--- chana/urls.py
+++ chana/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
 from django.conf.urls import url,include
 from django.contrib import admin
 
-from .views import index
+from .views import HomeView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'',index,name='index'),
-    url(r'^index/',name='index'),
+    url(r'',HomeView.as_view(),name='index'),
+    url(r'^index/',HomeView.as_view(),name='index'),
     url(r'^accounts/',include('accounts.urls')),
 ]
--- chana/views.py
+++ chana/views.py
@@ -2,9 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 #@login_required(login_url='/accounts/login/')
-class HomeView(LoginRequiredMixin,TemplateView):
-    login_url="/accounts/login/"
-    redirect_field_name='redirect_to'
+class HomeView(TemplateView):
     template_name="index.html"
 
 
