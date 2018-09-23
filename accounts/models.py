311cc7226368d2a8361eeedf7b870a42a47c584c
TralahM <mmtiese@gmail.com>
2018-09-23 23:06:02 +0300

    New version

--- accounts/models.py
+++ accounts/models.py
@@ -9,11 +9,11 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Profile(models.Model):
     avatar=models.ImageField()
-    user=models.ForeignKey(User)
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
     country=models.CharField(max_length=40)
     city=models.CharField(max_length=40)
     phone=models.CharField(max_length=15)
-    sex=models.CharField()
+    sex=models.CharField(max_length=1,choices=((u"M",u"Male"),(u"F",u"Female")))
     birthdate=models.DateField()
     age=models.IntegerField()
     married=models.BooleanField()
